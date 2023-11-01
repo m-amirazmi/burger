ab1bc9604f6bb26cd7a45f55995d939a4a35f0c3
Muhamad Amir <m.amirazmi@hotmail.com>
2023-11-01 12:28:03 +0800

    add simple create read to db

--- api/src/controllers/burger.controller.js
+++ api/src/controllers/burger.controller.js
@@ -1,7 +1,11 @@
-export const createBurger = (req, res) => {
-  return res.send("Create Burger");
+import Burger from "../models/burger.model.js";
+
+export const createBurger = async (req, res) => {
+  const burger = await Burger.create(req.body);
+  return res.status(201).json({ message: "A burger created.", burger });
 };
 
-export const getAllBurger = (req, res) => {
-  return res.send("Get All Burger");
+export const getAllBurger = async (req, res) => {
+  const burgers = await Burger.find({});
+  return res.status(200).json({ message: "All burgers.", burgers });
 };
