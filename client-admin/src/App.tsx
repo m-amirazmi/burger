d83f25cf1ff73902fc2936fa67f863e00609be9f
Muhamad Amir <m.amirazmi@hotmail.com>
2023-11-02 17:21:09 +0800

    test Update on burger admin

--- client-admin/src/App.tsx
+++ client-admin/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Burger Admin App</h1>
+      <h1>Burger Admin App Updated</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
