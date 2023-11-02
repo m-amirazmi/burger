44a27a55d00d6934be7739584c09508e2c66f62c
Muhamad Amir <m.amirazmi@hotmail.com>
2023-11-02 11:50:33 +0800

    add remove all api

--- api/src/controllers/burger.controller.js
+++ api/src/controllers/burger.controller.js
@@ -4,7 +4,11 @@ export const getAllBurger = async (req, res, next) => {
   try {
     const burgers = await Burger.find({});
     const message = burgers.length > 0 ? "All burgers" : "No burger.";
-    return res.status(200).json({ message, value: burgers });
+    return res.status(200).json({
+      total: burgers.length,
+      message,
+      value: burgers,
+    });
   } catch (error) {
     error.object = "Burger";
     next(error);
@@ -15,12 +19,10 @@ export const getBurger = async (req, res, next) => {
   const { id } = req.params;
   try {
     const burger = await Burger.findById(id);
-    return res
-      .status(200)
-      .json({
-        message: `Burger ${burger.name} has been found.`,
-        value: burger,
-      });
+    return res.status(200).json({
+      message: `Burger ${burger.name} has been found.`,
+      value: burger,
+    });
   } catch (error) {
     error.object = "Burger";
     next(error);
@@ -60,14 +62,24 @@ export const updateBurger = async (req, res, next) => {
 
 export const removeBurger = async (req, res, next) => {
   const { id } = req.params;
-
-  console.log("removeburger", id);
-
   try {
     const burger = await Burger.findByIdAndDelete(id);
     return res.json({
       message: `Removed burger ${burger.name}`,
       value: burger,
+    });
+  } catch (error) {
+    error.object = "Burger";
+    next(error);
+  }
+};
+
+export const removeAllBurger = async (req, res, next) => {
+  try {
+    await Burger.deleteMany();
+    return res.json({
+      message: "All burger has been removed",
+      value: [],
     });
   } catch (error) {
     error.object = "Burger";
--- api/src/routes/burger.route.js
+++ api/src/routes/burger.route.js
@@ -3,16 +3,18 @@ import {
   createBurger,
   getAllBurger,
   getBurger,
+  removeAllBurger,
   removeBurger,
   updateBurger,
 } from "../controllers/burger.controller.js";
 
 const router = Router();
 
-router.get("/", getAllBurger);
 router.get("/:id", getBurger);
+router.get("/", getAllBurger);
 router.post("/", createBurger);
 router.put("/:id", updateBurger);
 router.delete("/:id", removeBurger);
+router.delete("/", removeAllBurger);
 
 export default router;
