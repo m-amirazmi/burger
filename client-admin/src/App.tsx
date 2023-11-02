a623db8fb818b51fbba1e1c4b2fa353f8dccb15c
Muhamad Amir <m.amirazmi@hotmail.com>
2023-11-02 18:52:37 +0800

    Add chakra ui

--- client-admin/src/App.tsx
+++ client-admin/src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Flex, Text } from "@chakra-ui/react";
 
-function App() {
-  const [count, setCount] = useState(0)
+interface IProps {}
 
+export const App: React.FC<IProps> = ({}) => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Burger Admin App Updated</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <Flex
+      bgColor="black"
+      w="100vw"
+      h="100vh"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Text fontSize="6xl" textColor="white">
+        Work In Progress...
+      </Text>
+    </Flex>
+  );
+};
