234266e0673178c21420acf07499e014dcba8b73
Muhamad Amir <m.amirazmi@hotmail.com>
2023-11-03 13:56:06 +0800

    Standardize environments

--- api/src/utils/env.js
+++ api/src/utils/env.js
@@ -1,24 +1,4 @@
-const ENVIRONMENT = "dev";
-
-const envLocal = {
+export default {
   PORT: process.env.PORT,
   DB: process.env.DB,
 };
-
-const envDev = {
-  PORT: process.env.PORT,
-  DB: process.env.DB_DEV,
-};
-
-const envProd = {
-  PORT: process.env.PORT,
-  DB: process.env.DB_PROD,
-};
-
-const envs = {
-  local: envLocal,
-  dev: envDev,
-  prod: envProd,
-};
-
-export default envs[ENVIRONMENT];
