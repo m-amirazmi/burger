bfebba22378572975edf9d33251475746570e3c5
Muhamad Amir <m.amirazmi@hotmail.com>
2023-11-03 11:29:27 +0800

    Add local, dev and prod env

--- api/src/utils/env.js
+++ api/src/utils/env.js
@@ -1,6 +1,24 @@
-const env = {
+const ENVIRONMENT = "dev";
+
+const envLocal = {
   PORT: process.env.PORT,
   DB: process.env.DB,
 };
 
-export default env;
+const envDev = {
+  PORT: process.env.PORT,
+  DB: process.env.DB_DEV,
+};
+
+const envProd = {
+  PORT: process.env.PORT,
+  DB: process.env.DB_PROD,
+};
+
+const envs = {
+  local: envLocal,
+  dev: envDev,
+  prod: envProd,
+};
+
+export default envs[ENVIRONMENT];
